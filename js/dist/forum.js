054603cffe85c454c192d0a2b6b7c0a615e37037
Lupan Vasile <66137225+Vascan5123@users.noreply.github.com>
2021-04-21 13:01:11 +0300

    3

--- js/dist/forum.js
+++ js/dist/forum.js
@@ -1,2 +1,344 @@
-module.exports=function(e){var t={};function n(r){if(t[r])return t[r].exports;var o=t[r]={i:r,l:!1,exports:{}};return e[r].call(o.exports,o,o.exports,n),o.l=!0,o.exports}return n.m=e,n.c=t,n.d=function(e,t,r){n.o(e,t)||Object.defineProperty(e,t,{enumerable:!0,get:r})},n.r=function(e){"undefined"!=typeof Symbol&&Symbol.toStringTag&&Object.defineProperty(e,Symbol.toStringTag,{value:"Module"}),Object.defineProperty(e,"__esModule",{value:!0})},n.t=function(e,t){if(1&t&&(e=n(e)),8&t)return e;if(4&t&&"object"==typeof e&&e&&e.__esModule)return e;var r=Object.create(null);if(n.r(r),Object.defineProperty(r,"default",{enumerable:!0,value:e}),2&t&&"string"!=typeof e)for(var o in e)n.d(r,o,function(t){return e[t]}.bind(null,o));return r},n.n=function(e){var t=e&&e.__esModule?function(){return e.default}:function(){return e};return n.d(t,"a",t),t},n.o=function(e,t){return Object.prototype.hasOwnProperty.call(e,t)},n.p="",n(n.s=8)}([function(e,t){e.exports=flarum.core.compat.extend},function(e,t){e.exports=flarum.core.compat["components/IndexPage"]},function(e,t){e.exports=flarum.core.compat["components/DiscussionsUserPage"]},function(e,t){e.exports=flarum.core.compat["components/Navigation"]},function(e,t){e.exports=flarum.core.compat["components/DiscussionList"]},function(e,t){e.exports=flarum.core.compat["components/DiscussionListItem"]},function(e,t){e.exports=flarum.core.compat["components/Button"]},function(e,t){e.exports=flarum.core.compat["utils/Drawer"]},function(e,t,n){"use strict";n.r(t);var r=n(0),o=n(1),a=n.n(o),s=n(2),c=n.n(s),i=(n(6),n(3)),l=n.n(i),u=(n(7),n(4)),p=n.n(u),d=n(5),f=n.n(d);app.initializers.add("vascan/digi-ui",(function(){function e(){if(app.current.matches(a.a))return m("a",{id:"menu_open_id",class:"Button Button--icon hasIcon",title:"Menu",active:"false",type:"button",onclick:t.bind(this)},m("i",{class:"fas fa-bars Button-icon"}))}function t(){var e=document.getElementsByClassName("IndexPage-nav")[0];e.classList.contains("openMenu_class")?e.classList.remove("openMenu_class"):e.classList.add("openMenu_class")}Object(r.extend)(a.a.prototype,"init",(function(){})),Object(r.extend)(a.a.prototype,"sidebarItems",(function(e){})),Object(r.extend)(a.a.prototype,"actionItems",(function(e){e.has("refresh")&&e.remove("refresh"),e.has("markAllAsRead")&&e.remove("markAllAsRead")})),Object(r.extend)(a.a.prototype,"viewItems",(function(e){e.has("sort")&&e.remove("sort"),e.add("jumbotron",m("div",{class:"jumbotron_class"})),e.add("Text_title_center_block",m("p",{class:"Text_title_center_block_class"},m("span",null," What's new")))})),Object(r.override)(l.a.prototype,"view",(function(){var t=app,n=t.history,r=t.pane;return m("div",{className:"Navigation ButtonGroup "+(this.attrs.className||""),onmouseenter:r&&r.show.bind(r),onmouseleave:r&&r.onmouseleave.bind(r)},n.canGoBack()?[this.getBackButton(),this.getPaneButton()]:[e(),this.getDrawerButton()])})),Object(r.extend)(f.a.prototype,"view",(function(e){var t;try{(app.current.matches(a.a)||app.current.matches(c.a))&&((t=e.children[2].attrs.className.split(" "))[0]+="--grid",e.children[2].attrs.className=t.join(" "),(t=e.children[2].children[0].attrs.className.split(" "))[0]+="--grid",e.children[2].children[0].attrs.className=t.join(" "),(t=e.children[2].children[2].attrs.className.split(" "))[0]+="--grid",e.children[2].children[2].attrs.className=t.join(" "),(t=e.children[2].children[2].children[0].attrs.className.split(" "))[0]+="--grid",e.children[2].children[2].children[0].attrs.className=t.join(" "))}catch(e){}})),Object(r.extend)(p.a.prototype,"view",(function(e){var t;try{(app.current.matches(a.a)||app.current.matches(c.a))&&((t=e.children[0].attrs.className.split(" "))[0]+="--grid",e.children[0].attrs.className=t.join(" "))}catch(e){}}))}))}]);
+module.exports =
+/******/ (function(modules) { // webpackBootstrap
+/******/ 	// The module cache
+/******/ 	var installedModules = {};
+/******/
+/******/ 	// The require function
+/******/ 	function __webpack_require__(moduleId) {
+/******/
+/******/ 		// Check if module is in cache
+/******/ 		if(installedModules[moduleId]) {
+/******/ 			return installedModules[moduleId].exports;
+/******/ 		}
+/******/ 		// Create a new module (and put it into the cache)
+/******/ 		var module = installedModules[moduleId] = {
+/******/ 			i: moduleId,
+/******/ 			l: false,
+/******/ 			exports: {}
+/******/ 		};
+/******/
+/******/ 		// Execute the module function
+/******/ 		modules[moduleId].call(module.exports, module, module.exports, __webpack_require__);
+/******/
+/******/ 		// Flag the module as loaded
+/******/ 		module.l = true;
+/******/
+/******/ 		// Return the exports of the module
+/******/ 		return module.exports;
+/******/ 	}
+/******/
+/******/
+/******/ 	// expose the modules object (__webpack_modules__)
+/******/ 	__webpack_require__.m = modules;
+/******/
+/******/ 	// expose the module cache
+/******/ 	__webpack_require__.c = installedModules;
+/******/
+/******/ 	// define getter function for harmony exports
+/******/ 	__webpack_require__.d = function(exports, name, getter) {
+/******/ 		if(!__webpack_require__.o(exports, name)) {
+/******/ 			Object.defineProperty(exports, name, { enumerable: true, get: getter });
+/******/ 		}
+/******/ 	};
+/******/
+/******/ 	// define __esModule on exports
+/******/ 	__webpack_require__.r = function(exports) {
+/******/ 		if(typeof Symbol !== 'undefined' && Symbol.toStringTag) {
+/******/ 			Object.defineProperty(exports, Symbol.toStringTag, { value: 'Module' });
+/******/ 		}
+/******/ 		Object.defineProperty(exports, '__esModule', { value: true });
+/******/ 	};
+/******/
+/******/ 	// create a fake namespace object
+/******/ 	// mode & 1: value is a module id, require it
+/******/ 	// mode & 2: merge all properties of value into the ns
+/******/ 	// mode & 4: return value when already ns object
+/******/ 	// mode & 8|1: behave like require
+/******/ 	__webpack_require__.t = function(value, mode) {
+/******/ 		if(mode & 1) value = __webpack_require__(value);
+/******/ 		if(mode & 8) return value;
+/******/ 		if((mode & 4) && typeof value === 'object' && value && value.__esModule) return value;
+/******/ 		var ns = Object.create(null);
+/******/ 		__webpack_require__.r(ns);
+/******/ 		Object.defineProperty(ns, 'default', { enumerable: true, value: value });
+/******/ 		if(mode & 2 && typeof value != 'string') for(var key in value) __webpack_require__.d(ns, key, function(key) { return value[key]; }.bind(null, key));
+/******/ 		return ns;
+/******/ 	};
+/******/
+/******/ 	// getDefaultExport function for compatibility with non-harmony modules
+/******/ 	__webpack_require__.n = function(module) {
+/******/ 		var getter = module && module.__esModule ?
+/******/ 			function getDefault() { return module['default']; } :
+/******/ 			function getModuleExports() { return module; };
+/******/ 		__webpack_require__.d(getter, 'a', getter);
+/******/ 		return getter;
+/******/ 	};
+/******/
+/******/ 	// Object.prototype.hasOwnProperty.call
+/******/ 	__webpack_require__.o = function(object, property) { return Object.prototype.hasOwnProperty.call(object, property); };
+/******/
+/******/ 	// __webpack_public_path__
+/******/ 	__webpack_require__.p = "";
+/******/
+/******/
+/******/ 	// Load entry module and return exports
+/******/ 	return __webpack_require__(__webpack_require__.s = "./forum.js");
+/******/ })
+/************************************************************************/
+/******/ ({
+
+/***/ "./forum.js":
+/*!******************!*\
+  !*** ./forum.js ***!
+  \******************/
+/*! no exports provided */
+/***/ (function(module, __webpack_exports__, __webpack_require__) {
+
+"use strict";
+__webpack_require__.r(__webpack_exports__);
+/* harmony import */ var _src_forum__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! ./src/forum */ "./src/forum/index.js");
+/* empty/unused harmony star reexport */
+
+/***/ }),
+
+/***/ "./src/forum/index.js":
+/*!****************************!*\
+  !*** ./src/forum/index.js ***!
+  \****************************/
+/*! no exports provided */
+/***/ (function(module, __webpack_exports__, __webpack_require__) {
+
+"use strict";
+__webpack_require__.r(__webpack_exports__);
+/* harmony import */ var flarum_extend__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! flarum/extend */ "flarum/extend");
+/* harmony import */ var flarum_extend__WEBPACK_IMPORTED_MODULE_0___default = /*#__PURE__*/__webpack_require__.n(flarum_extend__WEBPACK_IMPORTED_MODULE_0__);
+/* harmony import */ var flarum_components_IndexPage__WEBPACK_IMPORTED_MODULE_1__ = __webpack_require__(/*! flarum/components/IndexPage */ "flarum/components/IndexPage");
+/* harmony import */ var flarum_components_IndexPage__WEBPACK_IMPORTED_MODULE_1___default = /*#__PURE__*/__webpack_require__.n(flarum_components_IndexPage__WEBPACK_IMPORTED_MODULE_1__);
+/* harmony import */ var flarum_components_DiscussionsUserPage__WEBPACK_IMPORTED_MODULE_2__ = __webpack_require__(/*! flarum/components/DiscussionsUserPage */ "flarum/components/DiscussionsUserPage");
+/* harmony import */ var flarum_components_DiscussionsUserPage__WEBPACK_IMPORTED_MODULE_2___default = /*#__PURE__*/__webpack_require__.n(flarum_components_DiscussionsUserPage__WEBPACK_IMPORTED_MODULE_2__);
+/* harmony import */ var flarum_components_Button__WEBPACK_IMPORTED_MODULE_3__ = __webpack_require__(/*! flarum/components/Button */ "flarum/components/Button");
+/* harmony import */ var flarum_components_Button__WEBPACK_IMPORTED_MODULE_3___default = /*#__PURE__*/__webpack_require__.n(flarum_components_Button__WEBPACK_IMPORTED_MODULE_3__);
+/* harmony import */ var flarum_components_Navigation__WEBPACK_IMPORTED_MODULE_4__ = __webpack_require__(/*! flarum/components/Navigation */ "flarum/components/Navigation");
+/* harmony import */ var flarum_components_Navigation__WEBPACK_IMPORTED_MODULE_4___default = /*#__PURE__*/__webpack_require__.n(flarum_components_Navigation__WEBPACK_IMPORTED_MODULE_4__);
+/* harmony import */ var flarum_components_DiscussionList__WEBPACK_IMPORTED_MODULE_5__ = __webpack_require__(/*! flarum/components/DiscussionList */ "flarum/components/DiscussionList");
+/* harmony import */ var flarum_components_DiscussionList__WEBPACK_IMPORTED_MODULE_5___default = /*#__PURE__*/__webpack_require__.n(flarum_components_DiscussionList__WEBPACK_IMPORTED_MODULE_5__);
+/* harmony import */ var flarum_components_DiscussionListItem__WEBPACK_IMPORTED_MODULE_6__ = __webpack_require__(/*! flarum/components/DiscussionListItem */ "flarum/components/DiscussionListItem");
+/* harmony import */ var flarum_components_DiscussionListItem__WEBPACK_IMPORTED_MODULE_6___default = /*#__PURE__*/__webpack_require__.n(flarum_components_DiscussionListItem__WEBPACK_IMPORTED_MODULE_6__);
+
+
+
+
+
+
+
+app.initializers.add('vascan/digi-ui', function () {
+  Object(flarum_extend__WEBPACK_IMPORTED_MODULE_0__["extend"])(flarum_components_IndexPage__WEBPACK_IMPORTED_MODULE_1___default.a.prototype, 'init', function () {
+    /* this.gridLayout = m.prop(false); */
+  });
+  Object(flarum_extend__WEBPACK_IMPORTED_MODULE_0__["extend"])(flarum_components_IndexPage__WEBPACK_IMPORTED_MODULE_1___default.a.prototype, 'sidebarItems', function (items) {// Убераем левое меню
+
+    /* if (items.has('newDiscussion')) {
+      items.remove('newDiscussion');
+    }
+    if (items.has('nav')) {
+      items.remove('nav');
+    } */
+  });
+  Object(flarum_extend__WEBPACK_IMPORTED_MODULE_0__["extend"])(flarum_components_IndexPage__WEBPACK_IMPORTED_MODULE_1___default.a.prototype, 'actionItems', function (items) {
+    // Stergerea butoanelor existente
+    if (items.has('refresh')) {
+      items.remove('refresh');
+    }
+
+    if (items.has('markAllAsRead')) {
+      items.remove('markAllAsRead');
+    }
+  });
+  Object(flarum_extend__WEBPACK_IMPORTED_MODULE_0__["extend"])(flarum_components_IndexPage__WEBPACK_IMPORTED_MODULE_1___default.a.prototype, 'viewItems', function (items) {
+    // Удаление кнопки сортировки
+    if (items.has('sort')) {
+      items.remove('sort');
+    } // Добавляем блок jumbotron сверху
+
+
+    items.add('jumbotron', m("div", {
+      "class": "jumbotron_class"
+    })); // Добавляем текст сверху
+
+    items.add('Text_title_center_block', m("p", {
+      "class": "Text_title_center_block_class"
+    }, m("span", null, " What's new")));
+  }); // Adding menu button
+
+  Object(flarum_extend__WEBPACK_IMPORTED_MODULE_0__["override"])(flarum_components_Navigation__WEBPACK_IMPORTED_MODULE_4___default.a.prototype, 'view', function () {
+    var _app = app,
+        history = _app.history,
+        pane = _app.pane;
+    return m("div", {
+      className: 'Navigation ButtonGroup ' + (this.attrs.className || ''),
+      onmouseenter: pane && pane.show.bind(pane),
+      onmouseleave: pane && pane.onmouseleave.bind(pane)
+    }, history.canGoBack() ? [this.getBackButton(), this.getPaneButton()] : [getMenuButton(), this.getDrawerButton()]);
+  }); // This is Menu button
+
+  function getMenuButton() {
+    if (app.current.matches(flarum_components_IndexPage__WEBPACK_IMPORTED_MODULE_1___default.a)) {
+      return m("a", {
+        id: "menu_open_id",
+        "class": "Button Button--icon hasIcon",
+        title: "Menu",
+        active: "false",
+        type: "button",
+        onclick: menuOn.bind(this)
+      }, m("i", {
+        "class": "fas fa-bars Button-icon"
+      }));
+    }
+  }
+
+  function menuOn() {
+    var nav = document.getElementsByClassName("IndexPage-nav")[0];
+
+    if (nav.classList.contains('openMenu_class')) {
+      nav.classList.remove("openMenu_class");
+    } else {
+      nav.classList.add("openMenu_class");
+    }
+  }
+
+  Object(flarum_extend__WEBPACK_IMPORTED_MODULE_0__["extend"])(flarum_components_DiscussionListItem__WEBPACK_IMPORTED_MODULE_6___default.a.prototype, 'view', function (view) {
+    var elementClassName;
+
+    try {
+      if (app.current.matches(flarum_components_IndexPage__WEBPACK_IMPORTED_MODULE_1___default.a) || app.current.matches(flarum_components_DiscussionsUserPage__WEBPACK_IMPORTED_MODULE_2___default.a)) {
+        // Modificare clasa DiscussionListItem-content
+        elementClassName = view.children[2].attrs.className.split(' ');
+        elementClassName[0] += '--grid';
+        view.children[2].attrs.className = elementClassName.join(' '); // Modificare clasa DiscussionListItem-author
+
+        elementClassName = view.children[2].children[0].attrs.className.split(' ');
+        elementClassName[0] += '--grid';
+        view.children[2].children[0].attrs.className = elementClassName.join(' '); // Modificare clasa DiscussionListItem-main
+
+        elementClassName = view.children[2].children[2].attrs.className.split(' ');
+        elementClassName[0] += '--grid';
+        view.children[2].children[2].attrs.className = elementClassName.join(' '); // Modificare clasa DiscussionListItem-title
+
+        elementClassName = view.children[2].children[2].children[0].attrs.className.split(' ');
+        elementClassName[0] += '--grid';
+        view.children[2].children[2].children[0].attrs.className = elementClassName.join(' ');
+      }
+    } catch (_unused) {}
+  });
+  Object(flarum_extend__WEBPACK_IMPORTED_MODULE_0__["extend"])(flarum_components_DiscussionList__WEBPACK_IMPORTED_MODULE_5___default.a.prototype, 'view', function (view) {
+    var elementClassName; // Modificare clasa DiscussionList-discussions
+
+    try {
+      if (app.current.matches(flarum_components_IndexPage__WEBPACK_IMPORTED_MODULE_1___default.a) || app.current.matches(flarum_components_DiscussionsUserPage__WEBPACK_IMPORTED_MODULE_2___default.a)) {
+        elementClassName = view.children[0].attrs.className.split(' ');
+        elementClassName[0] += '--grid';
+        view.children[0].attrs.className = elementClassName.join(' ');
+      }
+    } catch (_unused2) {}
+  });
+  Object(flarum_extend__WEBPACK_IMPORTED_MODULE_0__["extend"])(flarum_components_IndexPage__WEBPACK_IMPORTED_MODULE_1___default.a.prototype, 'actionItems', function (items) {
+    // Stergerea butoanelor existente
+    if (items.has('refresh')) {
+      items.remove('refresh');
+    }
+
+    if (items.has('markAllAsRead')) {
+      items.remove('markAllAsRead');
+    } // Adauga butonul pentru modificarea vizualizarii
+
+
+    items.add('gridLayout', flarum_components_Button__WEBPACK_IMPORTED_MODULE_3___default.a.component({
+      title: this.gridLayout() ? app.translator.trans('list-grid-layouts.forum.index.list_tooltip') : app.translator.trans('list-grid-layouts.forum.index.grid_tooltip'),
+      icon: this.gridLayout() ? 'fas fa-list-ul' : 'fas fa-th-large',
+      className: 'Button Button--icon',
+      onclick: modifyLayout.bind(this)
+    }));
+  });
+});
+
+/***/ }),
+
+/***/ "flarum/components/Button":
+/*!**********************************************************!*\
+  !*** external "flarum.core.compat['components/Button']" ***!
+  \**********************************************************/
+/*! no static exports found */
+/***/ (function(module, exports) {
+
+module.exports = flarum.core.compat['components/Button'];
+
+/***/ }),
+
+/***/ "flarum/components/DiscussionList":
+/*!******************************************************************!*\
+  !*** external "flarum.core.compat['components/DiscussionList']" ***!
+  \******************************************************************/
+/*! no static exports found */
+/***/ (function(module, exports) {
+
+module.exports = flarum.core.compat['components/DiscussionList'];
+
+/***/ }),
+
+/***/ "flarum/components/DiscussionListItem":
+/*!**********************************************************************!*\
+  !*** external "flarum.core.compat['components/DiscussionListItem']" ***!
+  \**********************************************************************/
+/*! no static exports found */
+/***/ (function(module, exports) {
+
+module.exports = flarum.core.compat['components/DiscussionListItem'];
+
+/***/ }),
+
+/***/ "flarum/components/DiscussionsUserPage":
+/*!***********************************************************************!*\
+  !*** external "flarum.core.compat['components/DiscussionsUserPage']" ***!
+  \***********************************************************************/
+/*! no static exports found */
+/***/ (function(module, exports) {
+
+module.exports = flarum.core.compat['components/DiscussionsUserPage'];
+
+/***/ }),
+
+/***/ "flarum/components/IndexPage":
+/*!*************************************************************!*\
+  !*** external "flarum.core.compat['components/IndexPage']" ***!
+  \*************************************************************/
+/*! no static exports found */
+/***/ (function(module, exports) {
+
+module.exports = flarum.core.compat['components/IndexPage'];
+
+/***/ }),
+
+/***/ "flarum/components/Navigation":
+/*!**************************************************************!*\
+  !*** external "flarum.core.compat['components/Navigation']" ***!
+  \**************************************************************/
+/*! no static exports found */
+/***/ (function(module, exports) {
+
+module.exports = flarum.core.compat['components/Navigation'];
+
+/***/ }),
+
+/***/ "flarum/extend":
+/*!***********************************************!*\
+  !*** external "flarum.core.compat['extend']" ***!
+  \***********************************************/
+/*! no static exports found */
+/***/ (function(module, exports) {
+
+module.exports = flarum.core.compat['extend'];
+
+/***/ })
+
+/******/ });
 //# sourceMappingURL=forum.js.map
--- js/src/forum/index.js
+++ js/src/forum/index.js
@@ -3,7 +3,6 @@ import IndexPage from 'flarum/components/IndexPage';
 import DiscussionsUserPage from 'flarum/components/DiscussionsUserPage';
 import Button from 'flarum/components/Button';
 import Navigation from 'flarum/components/Navigation';
-import Drawer from 'flarum/utils/Drawer';
 import DiscussionList from 'flarum/components/DiscussionList';
 import DiscussionListItem from 'flarum/components/DiscussionListItem';
 
@@ -43,7 +42,6 @@ app.initializers.add('vascan/digi-ui', () => {
       'Text_title_center_block',
       <p class="Text_title_center_block_class"><span> What's new</span></p>)
   });
-
   // Adding menu button
   override(Navigation.prototype, 'view', function () {
     const { history, pane } = app;
@@ -58,17 +56,15 @@ app.initializers.add('vascan/digi-ui', () => {
     );
   });
   // This is Menu button
-  
-    function getMenuButton() {
-      if (app.current.matches(IndexPage)) {
+  function getMenuButton() {
+    if (app.current.matches(IndexPage)) {
       return (
         <a id="menu_open_id" class="Button Button--icon hasIcon" title="Menu" active="false" type="button" onclick={menuOn.bind(this)}>
           <i class="fas fa-bars Button-icon"></i>
         </a>
       );
-      }
     }
-
+  }
   function menuOn() {
     let nav = document.getElementsByClassName("IndexPage-nav")[0];
     if (nav.classList.contains('openMenu_class')) {
@@ -77,11 +73,10 @@ app.initializers.add('vascan/digi-ui', () => {
       nav.classList.add("openMenu_class");
     }
   }
-
   extend(DiscussionListItem.prototype, 'view', function (view) {
     let elementClassName;
     try {
-      if (app.current.matches(IndexPage)||(app.current.matches(DiscussionsUserPage))) {
+      if (app.current.matches(IndexPage) || (app.current.matches(DiscussionsUserPage))) {
         // Modificare clasa DiscussionListItem-content
         elementClassName = view.children[2].attrs.className.split(' ');
         elementClassName[0] += '--grid';
@@ -110,13 +105,36 @@ app.initializers.add('vascan/digi-ui', () => {
     let elementClassName;
     // Modificare clasa DiscussionList-discussions
     try {
-      if (app.current.matches(IndexPage)||(app.current.matches(DiscussionsUserPage))) {
-      elementClassName = view.children[0].attrs.className.split(' ');
-      elementClassName[0] += '--grid';
-      view.children[0].attrs.className = elementClassName.join(' ');
+      if (app.current.matches(IndexPage) || (app.current.matches(DiscussionsUserPage))) {
+        elementClassName = view.children[0].attrs.className.split(' ');
+        elementClassName[0] += '--grid';
+        view.children[0].attrs.className = elementClassName.join(' ');
       }
     } catch {
 
     }
   });
+
+  extend(IndexPage.prototype, 'actionItems', function (items) {
+    // Stergerea butoanelor existente
+    if (items.has('refresh')) {
+      items.remove('refresh');
+    }
+    if (items.has('markAllAsRead')) {
+      items.remove('markAllAsRead');
+    }
+
+    // Adauga butonul pentru modificarea vizualizarii
+    items.add(
+      'gridLayout',
+      Button.component({
+        title: this.gridLayout() ? app.translator.trans('list-grid-layouts.forum.index.list_tooltip') : app.translator.trans('list-grid-layouts.forum.index.grid_tooltip'),
+        icon: this.gridLayout() ? 'fas fa-list-ul' : 'fas fa-th-large',
+        className: 'Button Button--icon',
+        onclick: modifyLayout.bind(this),
+      })
+    );
+  });
+
+
 });
